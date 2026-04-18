4bffaf645a961bc2181be20525bb019712a41388
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-04-18 21:21:23 +0600

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/the-grove-bistro/',
   plugins: [
     react(),
     tailwindcss(),
